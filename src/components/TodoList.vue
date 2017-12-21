51ca2e570e97ab6db17e74f9a30920f16d4f11b2
jozlnr <joostzoellner@online.de>
2017-12-22 00:50:57 +0100

    Changed button behaviour when closing or reopening a todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,13 +2,16 @@
     <div>
         <TodoInput/>
         <ul>
-            <li  class="box" v-for="todo in todos" v-bind:key="todo.id" v-bind:class="{ 'done': todo.done }">
+            <li class="box" v-for="todo in todos" v-bind:key="todo.id" v-bind:class="{ 'done': todo.done }">
                 <div class="level">
                     <div class="level-left">
                         {{ todo.name }}
                     </div>
                     <div class="level-right">
-                        <a class="button" @click="toggleTodo(todo)">Toggle</a>
+                        <a class="button" @click="toggleTodo(todo)" v-bind:class="{'is-warning': todo.done, 'is-success' : !todo.done}">
+                            <span v-if="todo.done">Reopen</span>
+                            <span v-else>Done</span>
+                        </a>
                     </div>
                 </div>
             </li>
@@ -23,9 +26,11 @@ import TodoInput from '@/components/TodoInput'
 
 export default {
     name: 'TodoList',
-    computed: mapGetters({
-        todos: 'getTodos'
-    }),
+    computed: {
+        ...mapGetters({
+            todos: 'getTodos'
+        })
+    },
     methods: {
         toggleTodo(todo) {
             this.$store.dispatch('TOGGLE_TODO', todo)
